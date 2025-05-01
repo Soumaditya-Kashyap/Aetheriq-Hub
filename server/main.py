21caac2d0a0262a0c7a04dba02cc21db1f3e473a
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-01 17:02:40 +0530

    added services for web search

--- server/main.py
+++ server/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from pydantic_model.chat_body import ChatBody
+from services.search_service import SearchService
 
 app = FastAPI()
 
+search_service = SearchService()
 # chatting
 @app.post('/chat')
 def chat_endpoint(body: ChatBody):
-	print(body.query)
+	#The main workflow
+	# 1. search the web and find out appropriate sources
+	# 2. sort the sources based on the relevance to the query
+	# 3. generate the response using LLM(Gemini)
 	return body.query
