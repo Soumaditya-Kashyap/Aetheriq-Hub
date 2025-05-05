960050d19507bc7d167c963277f816df46f29d26
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-06 01:12:21 +0530

    added tavily package and searched ownself, got correct results

--- server/services/search_service.py
+++ server/services/search_service.py
@@ -1,7 +1,11 @@
 from config import Settings
+from tavily import TavilyClient
 
 settings = Settings()
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
+
 
 class  SearchService:
     def web_search(self, query: str):
-        print(Settings().TAVILY_API_KEY)
+        response = tavily_client.search(query, max_results = 10)
+        print(response.get("results", []))
