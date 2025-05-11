732a43ff3ef6c4cffcf2ddb848c24586df22c01b
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-11 19:15:20 +0530

    UpStart

--- server/main.py
+++ server/main.py
@@ -12,11 +12,10 @@ sort_source_service= SortSourceService()
 # chatting
 @app.post('/chat')
 def chat_endpoint(body: ChatBody):
-	# 1. search the web and find out appropriate sources
+	
 	search_results = search_service.web_search(body.query)
 
 	sorted_results= sort_source_service.sort_sources(body.query, search_results)
 	print(sorted_results)
-	# 2. sort the sources based on the relevance to the query
-	# 3. generate the response using LLM(Gemini)
+	
 	return body.query
